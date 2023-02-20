da633a60e21c710b52aac79eee5b34879aa6c4f0
Raditss <raditya.naufal.a@gmail.com>
2023-02-20 22:08:27 +0700

    update kernel

--- src/kernel.c
+++ src/kernel.c
@@ -7,31 +7,23 @@
 
 void kernel_setup(void) {
     framebuffer_clear();
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_write(4, 8,  'S', 0, 0xF);
-    framebuffer_write(4, 9,  'e', 0, 0xF);
-    framebuffer_write(4, 10, 'l', 0, 0xF);
-    framebuffer_write(4, 11, 'a', 0, 0xF);
-    framebuffer_write(4, 12,  'm', 0, 0xF);
-    framebuffer_write(4, 13,  'a', 0, 0xF);
-    framebuffer_write(4, 14, 't', 0, 0xF);
-    framebuffer_write(5, 8, 'd', 0, 0xF);
-    framebuffer_write(5, 9, 'a', 0, 0xF);
-    framebuffer_write(5, 10, 't', 0, 0xF);
-    framebuffer_write(5, 11, 'a', 0, 0xF);
-    framebuffer_write(5, 12, 'n', 0, 0xF);
-    framebuffer_write(5, 13, 'g', 0, 0xF);
-    framebuffer_write(6, 8, 'd', 0, 0xF);
-    framebuffer_write(6, 9, 'i', 0, 0xF);
-    framebuffer_write(7, 8, 'm', 0, 0xF);
-    framebuffer_write(7, 9, 'a', 0, 0xF);
-    framebuffer_write(7, 10, 'm', 0, 0xF);
-    framebuffer_write(7, 11, 'p', 0, 0xF);
-    framebuffer_write(7, 12, 'O', 0, 3);
-    framebuffer_write(7, 13, 'S', 0, 3);    
+    framebuffer_write(3, 8,  ' ', 0, 0x1);
+    framebuffer_write(3, 9,  ' ', 0, 0x2);
+    framebuffer_write(3, 10, ' ', 0, 0x3);
+    framebuffer_write(3, 11, ' ', 0, 0x4);
+    framebuffer_write(4, 8,  ' ', 0, 0x5);
+    framebuffer_write(4, 9,  ' ', 0, 0x6);
+    framebuffer_write(4, 10, ' ', 0, 0x7);
+    framebuffer_write(4, 11, ' ', 0, 0x8);
+    framebuffer_write(4, 12,  ' ', 0, 0x9);
+    framebuffer_write(4, 13,  ' ', 0, 0xA);
+    framebuffer_write(4, 14, ' ', 0, 0xB);
+    framebuffer_write(5, 8, ' ', 0, 0xC);
+    framebuffer_write(5, 9, ' ', 0, 0xD);
+    framebuffer_write(5, 10, ' ', 0, 0xE);
+    framebuffer_write(5, 11, ' ', 0, 0xF);
+    framebuffer_write(5, 12, ' ', 0, 0x0);
+
 
     framebuffer_set_cursor(7, 13);
 
