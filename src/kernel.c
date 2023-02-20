c629b2fc59ba51c7bb7a6694edf38f0999c15251
Raditss <raditya.naufal.a@gmail.com>
2023-02-20 22:12:00 +0700

    simple art

--- src/kernel.c
+++ src/kernel.c
@@ -23,6 +23,14 @@ void kernel_setup(void) {
     framebuffer_write(5, 10, ' ', 0, 0xE);
     framebuffer_write(5, 11, ' ', 0, 0xF);
     framebuffer_write(5, 12, ' ', 0, 0x0);
+    framebuffer_write(8, 30, ':', 0xE, 0x0); // left eye
+    framebuffer_write(8, 32, ':', 0xE, 0x0); // right eye
+    framebuffer_write(9, 31, '.', 0xE, 0x0); // nose
+    framebuffer_write(10, 29, '\\', 0x7, 0xE); // left cheek
+    framebuffer_write(10, 34, '/', 0x7, 0xE); // right cheek
+    framebuffer_write(11, 30, '(', 0x0, 0xE); // mouth
+    framebuffer_write(11, 31, '_', 0x0, 0xE);
+    framebuffer_write(11, 32, ')', 0x0, 0xE);
 
 
     framebuffer_set_cursor(7, 13);
