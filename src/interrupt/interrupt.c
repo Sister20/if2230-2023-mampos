42743c1394ef8bc21e62cd6de095cf4b4bc58d25
ditramadia <ditrarizqaamadia@gmail.com>
2023-03-25 22:23:06 +0700

    feat: add intsetup

--- src/interrupt/interrupt.c
+++ src/interrupt/interrupt.c
@@ -1,6 +1,5 @@
 #include "interrupt.h"
 
-
 // Activate PIC mask for keyboard only
 void activate_keyboard_interrupt(void)
 {
@@ -10,14 +9,16 @@ void activate_keyboard_interrupt(void)
 }
 
 // I/O port wait, around 1-4 microseconds, for I/O synchronization putpose
-void io_wait(void) {
+void io_wait(void)
+{
     out(0x80, 0);
 }
 
 // Send ACK to PIC - @param irq Interrupt request number destination, note: this function already include PIC1_OFFSET
 void pic_ack(uint8_t irq)
 {
-    if (irq >= PIC1_OFFSET) {
+    if (irq >= PIC1_OFFSET)
+    {
         outb(PIC2_COMMAND, PIC_ACK);
     }
     outb(PIC1_COMMAND, PIC_ACK);
@@ -29,11 +30,11 @@ void pic_remap(void)
     uint8_t a1, a2;
 
     // Save masks
-    a1 = in(PIC1_DATA); 
+    a1 = in(PIC1_DATA);
     a2 = in(PIC2_DATA);
 
     // Starts the initialization sequence in cascade mode
-    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4); 
+    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
     io_wait();
     out(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
     io_wait();
@@ -41,9 +42,9 @@ void pic_remap(void)
     io_wait();
     out(PIC2_DATA, PIC2_OFFSET); // ICW2: Slave PIC vector offset
     io_wait();
-    out(PIC1_DATA, 0b0100);      // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+    out(PIC1_DATA, 0b0100); // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
     io_wait();
-    out(PIC2_DATA, 0b0010);      // ICW3: tell Slave PIC its cascade identity (0000 0010)
+    out(PIC2_DATA, 0b0010); // ICW3: tell Slave PIC its cascade identity (0000 0010)
     io_wait();
 
     out(PIC1_DATA, ICW4_8086);
@@ -54,21 +55,21 @@ void pic_remap(void)
     // Restore masks
     out(PIC1_DATA, a1);
     out(PIC2_DATA, a2);
-
 }
 
-void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack *frame)
+void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack frame)
 {
     // Call respective ISR based on interrupt number
-    switch (int_number) {
-        case IRQ_TIMER:
-            timer_interrupt_handler(cpu, frame);
-            break;
-        case IRQ_KEYBOARD:
-            keyboard_interrupt_handler(cpu, frame);
-            break;
-        case IRQ_MOUSE:
-            mouse_interrupt_handler(cpu, frame);
-            break;
+    switch (int_number)
+    {
+    case IRQ_TIMER:
+        timer_interrupt_handler(cpu, frame);
+        break;
+    case IRQ_KEYBOARD:
+        keyboard_interrupt_handler(cpu, frame);
+        break;
+    case IRQ_MOUSE:
+        mouse_interrupt_handler(cpu, frame);
+        break;
     }
 }
