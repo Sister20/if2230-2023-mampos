0e687b7e110b246a13dd74740785e92f2e9928d9
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-03-25 22:20:26 +0700

    fixed interrupt.c

--- src/interrupt/interrupt.c
+++ src/interrupt/interrupt.c
@@ -28,29 +28,33 @@ void pic_remap(void)
 {
     uint8_t a1, a2;
 
-    a1 = inb(PIC1_DATA);                        // save masks
-    a2 = inb(PIC2_DATA);
+    // Save masks
+    a1 = in(PIC1_DATA); 
+    a2 = in(PIC2_DATA);
 
-    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+    // Starts the initialization sequence in cascade mode
+    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4); 
     io_wait();
-    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    out(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
     io_wait();
-    outb(PIC1_DATA, PIC1_OFFSET);               // ICW2: Master PIC vector offset
+    out(PIC1_DATA, PIC1_OFFSET); // ICW2: Master PIC vector offset
     io_wait();
-    outb(PIC2_DATA, PIC2_OFFSET);               // ICW2: Slave PIC vector offset
+    out(PIC2_DATA, PIC2_OFFSET); // ICW2: Slave PIC vector offset
     io_wait();
-    outb(PIC1_DATA, 4);                         // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+    out(PIC1_DATA, 0b0100);      // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
     io_wait();
-    outb(PIC2_DATA, 2);                         // ICW3: tell Slave PIC its cascade identity (0000 0010)
-    io_wait();
-
-    outb(PIC1_DATA, ICW4_8086);
-    io_wait();
-    outb(PIC2_DATA, ICW4_8086);
+    out(PIC2_DATA, 0b0010);      // ICW3: tell Slave PIC its cascade identity (0000 0010)
     io_wait();
 
-    outb(PIC1_DATA, a1);   // restore saved masks.
-    outb(PIC2_DATA, a2);
+    out(PIC1_DATA, ICW4_8086);
+    io_wait();
+    out(PIC2_DATA, ICW4_8086);
+    io_wait();
+
+    // Restore masks
+    out(PIC1_DATA, a1);
+    out(PIC2_DATA, a2);
+
 }
 
 void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack *frame)
